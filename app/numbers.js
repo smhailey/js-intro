14353311a751c89f8f05a42a6f1eaa83a0448fe6
smhailey <smhailey@msn.com>
2019-06-11 13:18:18 -0600

    5 correct answers

--- app/numbers.js
+++ app/numbers.js
@@ -11,28 +11,40 @@ let sum = one + two + three;
 
 // 2. write a loop below that starts at 20 and ends at 100. Check each number,
 // if the number is divisible by 3, add it to the 'threes' array.
-let threes = []
-
-for (let i = 20; i <= 100; i++) {
-  if (i % 3 === 0) {
-    threes.push(i);
+function divisibleByThree() {
+  let threes = []
+  for (let i = 20; i <= 100; i++) {
+    if (i % 3 == 0) {
+      threes.push(i);
+    }
   }
-  alert(threes);
+  return threes
+}
 
 
 
 
 
-  // ------------------------------------------
+// ------------------------------------------
 
 
-  // 3. write code to find the class average given the following test scores
+// 3. write code to find the class average given the following test scores
 
-  let scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98];
+let scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98];
+let scores1 = [88, 84, 100, 92, 70, 76];
+let scores2 = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98];
+let scores3 = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98];
 
-  // code below
+
+// code below
+function findAverage(array) {
+
   let total = 0;
-  for (let i = 0; i < scores.length; i++) {
-    total += scores[i];
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
   }
-  let average = total / scores.length;
+  let average = (total / array.length)
+  return average
+}
+
+findAverage(scores3)
