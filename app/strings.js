ba2958446bdc7967e0ccf4703e618eda82f961c4
smhailey <smhailey@msn.com>
2019-06-13 16:33:47 -0600

    15 answers correct

--- app/strings.js
+++ app/strings.js
@@ -23,7 +23,13 @@ function uppercase(str) {
 let jsHistory = 'JavaScript, not to be confused with Java, was created in 10 days in May 1995 by Brendan Eich, then working at Netscape and now of Mozilla. JavaScript was not always known as JavaScript: the original name was Mocha, a name chosen by Marc Andreessen, founder of Netscape. In September of 1995 the name was changed to LiveScript, then in December of the same year, upon receiving a trademark license from Sun, the name JavaScript was adopted. This was somewhat of a marketing move at the time, with Java being very popular around then.';
 
 function letterCount(str, letter) {
-
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() == letter.toLowerCase()) {
+      count++;
+    }
+  }
+  return count;
 }
 
 
